bb3f5af3ff9af7392e8b35f3a4dfe312b3858345
Hector Perez M <hector.perez.m@usach.cl>
2019-09-07 15:33:26 -0400

    avance funciones secuenciales

--- lab_1/include/functions.h
+++ lab_1/include/functions.h
@@ -3,9 +3,13 @@
 
 /* functions file */
 void start(char *input_file, char *output_secuential, char *output_simd, int nflag, int dflag);
-void es_secuencial(char *input_file, char *output_secuential, int nflag, int dflag);
+void es_secuencial(char *input_file, char *output_secuential, int nflag, int dflag, int **kernel);
 void es_simd();
 int** createDataMatrix(int dimension);
+int** createKernel();
 void printResult(int dimension, int **data);
+int** secuential_kernel(int dimension, int **data, int **output, int **kernel);
+int** copyDataMatrix(int **data, int **output, int dimension);
+void writeResult(int nflag, int **data, char *name);
 
 #endif
